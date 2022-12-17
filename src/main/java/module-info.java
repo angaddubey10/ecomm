1a3696a72655373b782446e1445a124927cd8889
Angad Dubey <angaddubey@microsoft.com>
2022-12-17 15:33:16 +0530

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychainangad17dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychainangad17dec to javafx.fxml;
+    exports com.example.supplychainangad17dec;
+}
